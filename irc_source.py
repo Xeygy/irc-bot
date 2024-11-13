795e81887355dee47d1367b88ee730cc907dc785
Kevin Tan <k.tann.578@gmail.com>
2024-11-13 13:37:43 -0800

    has basic phase 1 functionality

--- irc_source.py
+++ irc_source.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import time
+import re
 
 class IRC:
     irc = socket.socket()
@@ -40,45 +41,70 @@ class IRC:
  
         return resp
 
-import os
-import random
-
 ## IRC Config
 server = "irc.libera.chat" 	# Provide a valid server IP/Hostname
 port = 6667
 channel = "#csc482"
-botnick = "test-bot"
+botnick = "hello-world-bot"
 botnickpass = ""		# in case you have a registered nickname 		
 botpass = ""			# in case you have a registered bot	
 
-def getSender(text):
+def getUsername(text):
     return text[1:text.index("!")]
+
+def getMessage(text):
+    return text[text.index(f"{botnick}:") + len(botnick) + 1:]
         
 def main():
     irc = IRC()
     irc.connect(server, port, channel, botnick, botpass, botnickpass)
+
+    currentUsers = set()
     
     while True:
         text = irc.get_response()
         print("RECEIVED ==> ",text) #:foaad-laptop!~foaad-lap@129.65.232.163 PRIVMSG foaad-bot :what's up?
-        
-        if "PRIVMSG" in text and channel in text and botnick+":" in text and ("hello" in text or "hi" in text):
-            irc.send(channel, f"{getSender(text)}: Hello World!")
-            
-        if "PRIVMSG" in text and channel in text and botnick+":" in text and ("who are you?" in text or "usage" in text):
-            irc.send(channel, f"{getSender(text)}: My name is {botnick}. I was created by Xiuyuan Qiu and Kevin Tan for CSC-482-01 and CSC-482-02.")
-            irc.send(channel, f"{getSender(text)}: I do not yet have a purpose or usage.")
 
-        if "PRIVMSG" in text and channel in text and botnick+":" in text and "users" in text:
-            irc.send(channel, f"{getSender(text)}: Bet")
-            
-        if "PRIVMSG" in text and channel in text and botnick+":" in text and "forget" in text:
-            irc.send(channel, f"{getSender(text)}: Bet")
-            
-        if "PRIVMSG" in text and channel in text and botnick+":" in text and "die" in text:
-            irc.send(channel, f"{getSender(text)}: Bet")
-            irc.command("QUIT")
-            sys.exit()
-        
+        if "PRIVMSG" not in text:
+            if "NAMES list" in text:
+                list = text[text.index(f"{channel} :") + len(channel) + 2:].split()
+                for user in list:
+                    if not re.search(r":[A-Za-z]+.libera.chat", user):
+                        currentUsers.add(user)
+                    else:
+                        break
+
+                print(currentUsers)
+            elif "JOIN" in text:
+                currentUsers.add(getUsername(text))
+            elif "QUIT" in text:
+                currentUsers.remove(getUsername(text))
+        else: # if "PRIVMSG" in text:
+            if channel in text and botnick+":" in text:
+                message = getMessage(text)
+
+                if ("hello" in message or "hi" in message):
+                    irc.send(channel, f"{getUsername(text)}: Hello World!")
+                
+                if ("who are you?" in message or "usage" in message):
+                    irc.send(channel, f"{getUsername(text)}: My name is {botnick}. I was created by Xiuyuan Qiu and Kevin Tan for CSC-482-01 and CSC-482-02.")
+                    irc.send(channel, f"{getUsername(text)}: I do not yet have a purpose or usage.")
+
+                if "users" in message:
+                    currentUsersStr = ""
+                    for user in sorted(currentUsers):
+                        currentUsersStr += user
+                        currentUsersStr += ", "
+                    irc.send(channel, f"{getUsername(text)}: {currentUsersStr[:-2]}")
+                    print(currentUsers)
+                    
+                if "forget" in message:
+                    irc.send(channel, f"{getUsername(text)}: Forgetting Everything")
+                    
+                if "die" in message:
+                    irc.send(channel, f"{getUsername(text)}: Bet")
+                    irc.command("QUIT")
+                    sys.exit()
+                
 if __name__=="__main__":
     main()
